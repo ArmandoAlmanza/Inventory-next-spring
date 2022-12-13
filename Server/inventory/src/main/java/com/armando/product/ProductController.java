b204ba4790e08bfa407a868c7b15a0367e1d148a
Armando Almanza <armando.s.almanza@gmail.com>
2022-12-12 18:05:57 -0600

    a

--- Server/inventory/src/main/java/com/armando/product/ProductController.java
+++ Server/inventory/src/main/java/com/armando/product/ProductController.java
@@ -3,7 +3,6 @@ package com.armando.product;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
